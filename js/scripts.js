6da99d7a790827bdeaec5d04bbdd046388014af0
spellcry <sangiothebest@hotmail.com>
2022-08-03 15:06:04 +0200

    add milestone 2

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,21 @@ function creaMembro(nome, ruolo, foto) {
 }
 
 // funzione che stampa un membro su console
-function stampaMembro(membro) {
+function stampaMembroConsole(membro) {
     console.log(`Nome: ${membro.nome} Ruolo: ${membro.ruolo} Foto: ${membro.foto}`);
 }
 
+// funzione che crea un elemento li
+function createLiElement(membro) {
+    return `<li>Nome: ${membro.nome} Ruolo: ${membro.ruolo} Foto: ${membro.foto}</li>`;
+}
+
+// funzione che aggiunge un membro in un elemento destinazione
+function addElementTo(elementoDestinazione, membro) {
+    elementoDestinazione.innerHTML += createLiElement(membro);
+}
+
+const teamEl = document.querySelector('.team');
 const team = [
     creaMembro('Wayne Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
     creaMembro('Angela Caroll', 'Chief Editor', 'angela-caroll-chief-editor.jpg'),
@@ -23,5 +34,6 @@ const team = [
 ];
 
 team.forEach((membro) => {
-    stampaMembro(membro);
+    stampaMembroConsole(membro);
+    addElementTo(teamEl, membro);
 });
